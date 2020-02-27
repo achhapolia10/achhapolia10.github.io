ad5132a5b509b0247a950359b4c1631a4dc28b2c
Anshuman Chhapolia <achhap.10.01@gmail.com>
2020-02-27 22:40:47 +0530

    Initial Commit
    Signed-off-by: Anshuman Chhapolia <achhap.10.01@gmail.com>

---
+++ precache-manifest.9b27969e87d5b9c2fd7cead743c846d6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03a1db03f0bc250629479b6c4a9b0d71",
+    "url": "/index.html"
+  },
+  {
+    "revision": "05d8555ffa3c0ff29e02",
+    "url": "/static/css/main.af041777.chunk.css"
+  },
+  {
+    "revision": "f4fff6f61606685bb1de",
+    "url": "/static/js/2.b33e57b7.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.b33e57b7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "05d8555ffa3c0ff29e02",
+    "url": "/static/js/main.b2f8b877.chunk.js"
+  },
+  {
+    "revision": "4186b7f7af39eb68ba2c",
+    "url": "/static/js/runtime-main.9415e33b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
